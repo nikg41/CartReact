614c7008784584422e990f28c33025c82ecc9597
Nikhil Garg <nikhil.garg@prudential.com.sg>
2022-01-20 13:15:13 +0530

    Code changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,23 @@
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
-
+import { useSelector } from 'react-redux';
 const Cart = (props) => {
+  const cartItems = useSelector(state => state.cart.items);
+  console.log(":::: cartItems", cartItems)
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map(item => (
+          <CartItem
+            item={{
+              title: item.title,
+              quantity: item.quantity,
+              total: item.quantity * item.price,
+              price: item.price
+            }}
+          />))}
       </ul>
     </Card>
   );
